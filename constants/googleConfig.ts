973ed83f5369f0325dc4bc790361e097289ee6a4
darui3018823 <151906210+darui3018823@users.noreply.github.com>
2026-05-02 22:14:12 +0900

    chore(auth): update google client ids

--- constants/googleConfig.ts
+++ constants/googleConfig.ts
@@ -1,2 +1,2 @@
-export const GOOGLE_IOS_CLIENT_ID = '[iOSのクライアントID]';
-export const GOOGLE_WEB_CLIENT_ID = '[ウェブのクライアントID]';
+export const GOOGLE_IOS_CLIENT_ID = '171480425592-epcnn61pcg7ncgn5oeslt85isbp1e3f5.apps.googleusercontent.com';
+export const GOOGLE_WEB_CLIENT_ID = '171480425592-5mutdd96kl48lf66aav9mp02fpgj41c2.apps.googleusercontent.com';
